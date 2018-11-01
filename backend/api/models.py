0aba777ffc4d5e3efc25a10692ad9b5babc3bf1c
pbzweihander <sd852456@naver.com>
2018-11-01 13:58:07 +0900

    party model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from enum import Enum
 
-# Create your models here.
+
+def enum_to_choice(enum: Enum):
+    return [(tag, tag.value) for tag in enum]
+
+
+class PartyType(Enum):
+    PublicParty = 0
+    GroupOpenedParty = 1
+    ClosedParty = 2
+
+
+class Party(models.Model):
+    name = models.CharField(max_length=120)
+    state = models.IntegerField()
+    type = models.SmallIntegerField(choices=enum_to_choice(PartyType))
+    location = models.CharField(max_length=120)
+    leader = models.ForeignKey(User, on_delete=models.CASCADE)
+    since = models.DateTimeField(auto_now=True)
