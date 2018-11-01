9cf60c5bad1c4f1b7ab16484af83d0e7f931d0be
pbzweihander <sd852456@naver.com>
2018-11-01 20:21:32 +0900

    party state model

--- backend/websocket/models.py
+++ backend/websocket/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from enum import Enum
 
-# Create your models here.
+
+class PartyPhase(Enum):
+    ChoosingRestaurant = 0
+    ChoosingMenu = 1
+    Ordering = 2
+    Ordered = 3
+    PaymentAndCollection = 4
+
+
+class PartyState:
+    def __init__(self, party_id: int):
+        self.key = 'party:{}'.format(party_id)
+        self.phase = PartyPhase.ChoosingRestaurant
+        self.restaurant = None
+        self.members = []
+        self.menus = {}
