fafbeda8efefafb741d7e62795e91f8dbfa4f097
pbzweihander <sd852456@naver.com>
2018-11-01 09:55:34 +0900

    updated testing configuration

--- util/ci-frontend.sh
+++ util/ci-frontend.sh
@@ -3,23 +3,19 @@
 set -e
 
 TYPE=$1
-if [ $TYPE = 'before_install' ]; then
-    echo '*** Running xvfb'
-    sh -e /etc/init.d/xvfb start
-elif [ $TYPE = 'install' ]; then
+if [ $TYPE = 'install' ]; then
     echo '*** Installing dependencies'
     cd frontend
     npm install
-    npm install coveralls
     cd ..
-elif [ $TYPE = 'before_script' ]; then
-    echo '*** Nothing to do'
 elif [ $TYPE = 'script' ]; then
     cd frontend
     echo '*** Running linter'
     npm run lint
     echo '*** Running unit tests'
-    npm run test -- --code-coverage --watch false
+    npm run test -- \
+        --watch false \
+        --browsers ChromeHeadless
     cd ..
 elif [ $TYPE = 'after_success' ]; then
     echo '*** Submitting coverage info'
