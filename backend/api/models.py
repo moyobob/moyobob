84a1f2404cc4ed51427c56010f8fb71ea7886448
pbzweihander <sd852456@naver.com>
2018-11-02 22:53:54 +0900

    use IntEnum

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.cache import cache
-from enum import Enum
+from enum import IntEnum
 
 
-def enum_to_choice(enum: Enum):
+def enum_to_choice(enum: IntEnum):
     return [(tag, tag.value) for tag in enum]
 
 
-class PartyType(Enum):
+class PartyType(IntEnum):
     InGroup = 0
     Private = 1
 
@@ -18,7 +18,7 @@ class Party(models.Model):
     type = models.SmallIntegerField(choices=enum_to_choice(PartyType))
     location = models.CharField(max_length=120)
     leader = models.ForeignKey(User, on_delete=models.CASCADE)
-    members = models.ManyToManyField(User)
+    members = models.ManyToManyField(User, related_name='parties')
     since = models.DateTimeField(auto_now=True)
 
     def member_count(self):
