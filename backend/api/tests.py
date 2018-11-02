e645827d64f1be3f670dfcb3db95dcfcbcced606
pbzweihander <sd852456@naver.com>
2018-11-02 23:42:51 +0900

    party view test

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase, Client
 from django.contrib.auth.models import User
 import json
 
+from .models import Party, PartyType
+
 
 class TestCaseWithHttp(TestCase):
     def setUp(self):
@@ -18,6 +20,15 @@ class TestCaseWithHttp(TestCase):
 
     def delete(self, url):
         return self.client.delete(url)
+
+    def login(self, email, password):
+        self.post('/api/signin/', {
+            'email': email,
+            'password': password,
+        })
+
+    def logout(self):
+        self.get('/api/signout/')
 
 
 class UserTestCase(TestCaseWithHttp):
@@ -63,3 +74,88 @@ class UserTestCase(TestCaseWithHttp):
     def test_bad_request(self):
         self.assertEqual(self.post('/api/signup/', {}).status_code, 400)
         self.assertEqual(self.post('/api/signin/', {}).status_code, 400)
+
+
+class PartyTestCase(TestCaseWithHttp):
+    def setUp(self):
+        super().setUp()
+
+        self.user = User.objects.create_user(
+            username='ferris', email='ferris@rustacean.org', password='iluvrust')
+
+        self.party1 = Party(
+            name="party 1 name",
+            type=int(PartyType.Private),
+            location="party 1 location",
+            leader=self.user,
+        )
+        self.party1.save()
+
+        self.party2 = Party(
+            name="party 2 name",
+            type=int(PartyType.InGroup),
+            location="party 2 location",
+            leader=self.user,
+        )
+        self.party2.save()
+
+    def login(self):
+        super().login(email='ferris@rustacean.org', password='iluvrust')
+
+    def test_not_allowd(self):
+        self.assertEqual(self.put('/api/party/', {}).status_code, 405)
+        self.assertEqual(self.delete('/api/party/').status_code, 405)
+
+    def test_unauthorized(self):
+        self.assertEqual(self.get('/api/party/').status_code, 403)
+        self.assertEqual(self.post('/api/party/', {}).status_code, 403)
+
+    def test_get_party(self):
+        self.login()
+
+        resp = self.get('/api/party/')
+        self.assertEqual(resp.status_code, 200)
+
+        resp_json = resp.json()
+        self.assertEqual(len(resp_json), 2)
+        self.assertEqual(resp_json[0]['name'], self.party1.name)
+        self.assertEqual(resp_json[0]['type'], self.party1.type)
+        self.assertEqual(resp_json[0]['location'], self.party1.location)
+        self.assertEqual(resp_json[0]['leader_id'], self.party1.leader.id)
+        self.assertEqual(resp_json[1]['name'], self.party2.name)
+        self.assertEqual(resp_json[1]['type'], self.party2.type)
+        self.assertEqual(resp_json[1]['location'], self.party2.location)
+        self.assertEqual(resp_json[1]['leader_id'], self.party2.leader.id)
+
+    def test_post_party(self):
+        self.login()
+
+        new_party = {
+            'name': 'new party name',
+            'type': PartyType.Private,
+            'location': 'new party location',
+        }
+
+        resp = self.post('/api/party/', new_party)
+        self.assertEqual(resp.status_code, 200)
+
+        resp_json = resp.json()
+        self.assertEqual(resp_json['name'], new_party['name'])
+        self.assertEqual(resp_json['type'], new_party['type'])
+        self.assertEqual(resp_json['location'], new_party['location'])
+        self.assertEqual(resp_json['leader_id'], self.user.id)
+        self.assertListEqual(resp_json['members'], [self.user.id])
+
+        id = resp_json['id']
+        party = Party.objects.get(id=id)
+        self.assertEqual(party.name, new_party['name'])
+        self.assertEqual(party.type, new_party['type'])
+        self.assertEqual(party.location, new_party['location'])
+        self.assertEqual(party.leader.id, self.user.id)
+        self.assertListEqual(list(party.members.all()), [self.user])
+
+    def test_post_party_bad_request(self):
+        self.login()
+
+        resp = self.post('/api/party/', {})
+        self.assertEqual(resp.status_code, 400)
