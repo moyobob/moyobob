d4fe1ceb8c39740c1715d55bd6d9ac6b4327550e
pbzweihander <sd852456@naver.com>
2018-11-03 21:24:22 +0900

    cache model superclass

--- backend/websocket/models.py
+++ backend/websocket/models.py
@@ -3,6 +3,28 @@ from enum import Enum
 import json
 
 
+class CacheModel():
+    _key = 'key'
+
+    @classmethod
+    def key(cls, id):
+        return '{}:{}'.format(cls._key, id)
+
+    @classmethod
+    def get(cls, id: int):
+        j = cache.get(cls.key(id))
+
+        o = cls(j['id'])
+        o.__dict__ = json.loads(j)
+        return o
+
+    def save(self):
+        cache.set(type(self).key(self.id), json.dumps(self))
+
+    def delete(self):
+        cache.delete(type(self).key(self.id))
+
+
 class PartyPhase(Enum):
     ChoosingRestaurant = 0
     ChoosingMenu = 1
@@ -11,17 +33,12 @@ class PartyPhase(Enum):
     PaymentAndCollection = 4
 
 
-class PartyState:
+class PartyState(CacheModel):
+    _key = 'party'
+
     def __init__(self, party_id: int):
-        self.key = 'party:{}'.format(party_id)
+        self.id = party_id
         self.phase = PartyPhase.ChoosingRestaurant
         self.restaurant = None
         self.members = []
         self.menus = {}
-
-    def save(self):
-        cache.set(self.key, json.dumps(self))
-
-    @classmethod
-    def get(party_id: int):
-        return cache.get('party:{}'.format(party_id))
