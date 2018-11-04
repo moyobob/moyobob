32918e1aaee3a7728aec2cc69cf9fb1f4fa74c63
ChooHyunSuk <cngustjr1235@naver.com>
2018-11-04 21:35:50 +0900

    add PartyType enum

--- frontend/src/app/types/party.ts
+++ frontend/src/app/types/party.ts
@@ -1,7 +1,12 @@
+enum PartyType {
+  InGroup = 0,
+  Private = 1,
+}
+
 export class Party {
   id: number;
   name: string;
-  type: number;
+  type: PartyType;
   location: string;
   leader_id: number;
   since: string;
