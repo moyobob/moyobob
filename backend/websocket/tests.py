fadab906e6540da7873fabf6dc65947ed8b78db5
pbzweihander <sd852456@naver.com>
2018-11-05 02:42:06 +0900

    test unauthenticated during connection

--- backend/websocket/tests.py
+++ backend/websocket/tests.py
@@ -57,6 +57,36 @@ class WebsocketTestCase(TestCaseWithCache):
         await communicator.disconnect()
 
     @async_test
+    async def test_unauthenticated_during_connection(self):
+        from django.contrib.auth.models import AnonymousUser
+
+        user = User.objects.create_user(
+            email='ferris@rustacean.org',
+            password='iluvrust',
+            username='ferris',
+        )
+        self.client.login(email=user.email, password='iluvrust')
+
+        communicator = WebsocketCommunicator(WebsocketConsumer, '/',)
+        communicator.scope['user'] = user
+
+        await communicator.connect()
+        await communicator.receive_from()
+
+        self.client.logout()
+        communicator.scope['user'] = AnonymousUser()
+
+        await communicator.send_json_to({
+            'command': 'party.join',
+            'foo': 'bar',
+        })
+        resp = await communicator.receive_output(1)
+        self.assertEqual(resp['type'], 'websocket.close')
+        self.assertEqual(resp['code'], 4010)
+
+        self.assertTrue(await communicator.receive_nothing())
+
+    @async_test
     async def test_invalid_command(self):
         user = User.objects.create_user(
             email='ferris@rustacean.org',
