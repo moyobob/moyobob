d7d6bb46022fc54f63a12abf1f1c37727e1abd04
pbzweihander <sd852456@naver.com>
2018-11-04 22:59:44 +0900

    test connect

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,6 +89,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 30
+        },
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'testdb.sqlite3'),
+        }
     }
 }
 
--- backend/websocket/tests.py
+++ backend/websocket/tests.py
@@ -1,9 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.core.cache import cache
 from django.contrib.auth.models import User
 from channels.testing import WebsocketCommunicator
+import asyncio
+from django.contrib.auth import login
 
 from backend.routings import application
+
+
+def async_test(f):
+    def wrapper(*args, **kwargs):
+        coro = asyncio.coroutine(f)
+        future = coro(*args, **kwargs)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(future)
+    return wrapper
 
 
 class TestCaseWithCache(TestCase):
@@ -16,14 +27,30 @@ class TestCaseWithCache(TestCase):
 
 
 class WebsocketTestCase(TestCaseWithCache):
-    def setUp(self):
-        super().setUp()
-        User.objects.create_user(
+    @async_test
+    async def test_unauthenticated(self):
+        communicator = WebsocketCommunicator(application, '/ws/party/')
+        resp = await communicator.connect(1)
+        self.assertTupleEqual(resp, (False, 4000))
+
+    @async_test
+    async def test_connect(self):
+        user = User.objects.create_user(
             email='ferris@rustacean.org',
             password='iluvrust',
             username='ferris',
         )
-        self.communicator = WebsocketCommunicator(application, '/ws/party/')
+        self.client.login(email=user.email, password='iluvrust')
 
-    def test_unauthenticated(self):
-        raise Exception("foo")
+        headers = [
+            (b'cookie', self.client.cookies.output(header='', sep='; ').encode())
+        ]
+        print(headers)
+        communicator = WebsocketCommunicator(
+            application, '/ws/party/', headers)
+
+        connected, code = await communicator.connect()
+        self.assertTrue(connected)
+        self.assertEqual(communicator.instance.scope['user'], user)
+
+        await communicator.disconnect()
