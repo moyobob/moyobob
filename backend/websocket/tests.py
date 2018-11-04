6023edb456af2ffc0381ca83bb2955f710b57992
pbzweihander <sd852456@naver.com>
2018-11-04 20:19:59 +0900

    websocket test skeleton

--- backend/websocket/tests.py
+++ backend/websocket/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.core.cache import cache
+from django.contrib.auth.models import User
+from channels.testing import WebsocketCommunicator
 
-# Create your tests here.
+from backend.routings import application
+
+
+class TestCaseWithCache(TestCase):
+    def setUp(self):
+        cache.clear()
+
+    def tearDown(self):
+        from django_redis import get_redis_connection
+        get_redis_connection('default').flushdb()
+
+
+class WebsocketTestCase(TestCaseWithCache):
+    def setUp(self):
+        super().setUp()
+        User.objects.create_user(
+            email='ferris@rustacean.org',
+            password='iluvrust',
+            username='ferris',
+        )
+        self.communicator = WebsocketCommunicator(application, '/ws/party/')
+
+    def test_unauthenticated(self):
+        raise Exception("foo")
