50a1a627ae686a659b62c6967ab2bae7a62f868f
pbzweihander <sd852456@naver.com>
2018-11-04 16:22:37 +0900

    use circleci instead of travis

--- util/ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $TEST = 'BACKEND' ]; then
-    echo "*** Configuration: Backend"
-    exec ./util/ci-backend.sh $1
-elif [ $TEST = 'FRONTEND' ]; then
-    echo "*** Configuration: Frontend"
-    exec ./util/ci-frontend.sh $1
-fi
