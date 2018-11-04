a3431c0900df914d84316ea50ec12b42e64675fe
kipa00 <k2pa00@gmail.com>
2018-11-04 23:42:22 +0900

    add requestSignIn

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  signedInUser: User;
+
+  constructor(private http: HttpClient) { }
+
+  requestSignIn(email: string, password: string) {
+    this.http.post('/api/signin/', {
+      'email': email,
+      'password': password,
+    }).toPromise().then(user => {
+      this.signedInUser = user;
+      return true;
+    }, error => {
+      return false;
+    });
+  }
+
 }
