84b41f46a2f2282d92d2c141e501155c26e82e1b
kipa00 <k2pa00@gmail.com>
2018-11-04 19:12:36 +0900

    make basic ui

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LobbyListItemComponent } from './lobby-list-item/lobby-list-item.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignInComponent,
     LobbyListItemComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
---
+++ frontend/src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+enum InputStatus {
+  HaveNotTriedSignIn,
+  IdNoInput,
+  PasswordNoInput,
+  TriedSignIn,
+}
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+
+export class SignInComponent implements OnInit {
+
+  idInput: string;
+  passwordInput: string;
+
+  logInStatus: InputStatus;
+
+  inputStatus = InputStatus;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.logInStatus = InputStatus.HaveNotTriedSignIn;
+  }
+
+  trySignIn(event) {
+    if (!event || event.key === "Enter") {
+      if (!this.idInput) {
+        this.logInStatus = InputStatus.IdNoInput;
+      } else if (!this.passwordInput) {
+        this.logInStatus = InputStatus.PasswordNoInput;
+      } else {
+        this.logInStatus = InputStatus.TriedSignIn;
+      }
+    }
+  }
+
+}
