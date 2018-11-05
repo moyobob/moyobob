3ab7d31262b04bcaa461117ab9cb6cee13761625
ChooHyunSuk <cngustjr1235@naver.com>
2018-11-05 14:47:45 +0900

    implemented get parties with testing

--- frontend/src/app/lobby/lobby.component.spec.ts
+++ frontend/src/app/lobby/lobby.component.spec.ts
@@ -1,16 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LobbyComponent } from './lobby.component';
+import {Party, PartyType} from '../types/party';
+import {PartyService} from '../services/party.service';
+import {FormsModule} from '@angular/forms';
+import {Component, Input} from '@angular/core';
+
+const mockParties: Party[] = [
+  {
+    id: 1,
+    name: 'Name 1',
+    type: PartyType.InGroup,
+    location: 'Location 1',
+    leaderId: 1,
+    since: 'Since 1',
+  },
+  {
+    id: 2,
+    name: 'Name 2',
+    type: PartyType.Private,
+    location: 'Location 2',
+    leaderId: 2,
+    since: 'Since 2',
+  },
+];
+
+@Component({selector: 'app-lobby-list-item', template: ''})
+export class MockLobbyListItemComponent {
+  @Input() party: Party;
+}
 
 describe('LobbyComponent', () => {
   let component: LobbyComponent;
   let fixture: ComponentFixture<LobbyComponent>;
+  let partyService: jasmine.SpyObj<PartyService>;
 
   beforeEach(async(() => {
+    const partySpy = jasmine.createSpyObj('PartyService', ['getParties']);
     TestBed.configureTestingModule({
-      declarations: [ LobbyComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        LobbyComponent,
+        MockLobbyListItemComponent
+      ],
+      providers: [
+        { provide: PartyService, useValue: partySpy }
+      ]
+    }).compileComponents();
+
+    partyService = TestBed.get(PartyService);
+    partyService.getParties.and.returnValue(new Promise(resolve => resolve(mockParties)));
   }));
 
   beforeEach(() => {
@@ -22,4 +60,13 @@ describe('LobbyComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get parties', async(() => {
+    component.ngOnInit();
+    fixture.whenStable().then( () => {
+      fixture.detectChanges();
+      expect(partyService.getParties).toHaveBeenCalled();
+      expect(component.parties).toEqual(mockParties);
+    });
+  }));
 });
--- frontend/src/app/lobby/lobby.component.ts
+++ frontend/src/app/lobby/lobby.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Party } from '../types/party';
+import { PartyService } from '../services/party.service';
 
 @Component({
   selector: 'app-lobby',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lobby.component.css']
 })
 export class LobbyComponent implements OnInit {
+  parties: Party[];
 
-  constructor() { }
+  constructor(
+    private partyService: PartyService,
+  ) { }
 
   ngOnInit() {
+    this.getParties();
   }
 
+  getParties(): void {
+    this.partyService.getParties().then(parties => this.parties = parties);
+  }
 }
