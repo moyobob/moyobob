6546d9cb9043eef2761e5fd472e59546f2f8432d
kipa00 <k2pa00@gmail.com>
2018-11-14 05:58:51 +0900

    add guard

--- frontend/src/app/app-routing/app-routing.module.ts
+++ frontend/src/app/app-routing/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SignInComponent } from '../sign-in/sign-in.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
-  {
-    path: 'sign-in',
-    component: SignInComponent
-  }
+  { path: 'sign-in', component: SignInComponent, canActivate: [AuthGuard] },
+  { path: 'party', component: SignInComponent, canActivate: [AuthGuard] },
+  { path: 'party/:id', component: SignInComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: '**', redirectTo: '/sign-in' },
 ];
 
 @NgModule({
