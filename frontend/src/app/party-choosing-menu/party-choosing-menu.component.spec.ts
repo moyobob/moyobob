83cb664219640a41edcf42cc3e81254ddb089832
kipa00 <k2pa00@gmail.com>
2018-11-18 17:00:05 +0900

    complete PartyChoosingMenuComponent test; coverage 100%

--- frontend/src/app/party-choosing-menu/party-choosing-menu.component.spec.ts
+++ frontend/src/app/party-choosing-menu/party-choosing-menu.component.spec.ts
@@ -5,13 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { PartyChoosingMenuComponent } from './party-choosing-menu.component';
 
-import { Menu, PartyMenu, PartyMenuCreateRequest } from '../types/menu';
+import {
+  Menu, PartyMenu, PartyMenuUpdateRequest, PartyMenuCreateRequest
+} from '../types/menu';
 import { PartyState } from '../types/party';
 
 import { UserService } from '../services/user.service';
 import { PartyService } from '../services/party.service';
 
+const mockPartyMenu1 = { id: 1, menuId: 1, quantity: 2, userIds: [1] };
+const mockPartyMenu2 = { id: 2, menuId: 2, quantity: 2, userIds: [1, 2] };
+const mockPartyMenu3 = { id: 3, menuId: 3, quantity: 1, userIds: [2] };
+
+const mockMenu1 = { id: 1, name: "Mock Menu 1", price: -120 };
+
 class MockUserService {
+  signedInUserId: number = 1;
 }
 
 class MockPartyService {
@@ -25,7 +34,13 @@ class MockPartyService {
     menus: []
   };
   getMenus() {
-    return new Promise(resolve => resolve([]));
+    return new Promise(resolve => resolve([mockMenu1]));
+  }
+  createMenu(partyMenuCreateRequest: PartyMenuCreateRequest) {
+
+  }
+  updateMenu(partyMenuUpdateRequest: PartyMenuUpdateRequest) {
+
   }
 }
 
@@ -40,6 +55,8 @@ class MockSelectMenuComponent {
 describe('PartyChoosingMenuComponent', () => {
   let component: PartyChoosingMenuComponent;
   let fixture: ComponentFixture<PartyChoosingMenuComponent>;
+
+  let mockPartyService: MockPartyService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -58,8 +75,25 @@ describe('PartyChoosingMenuComponent', () => {
     .compileComponents();
   }));
 
+  it('should not update menu when not preloaded', () => {
+    fixture = TestBed.createComponent(PartyChoosingMenuComponent);
+    mockPartyService = TestBed.get(PartyService);
+    mockPartyService.partyState = {
+      id: 1,
+      phase: 0,
+      restaurant: null,
+      members: [],
+      menus: undefined
+    };
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+  })
+
   beforeEach(() => {
     fixture = TestBed.createComponent(PartyChoosingMenuComponent);
+    mockPartyService = TestBed.get(PartyService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -67,4 +101,118 @@ describe('PartyChoosingMenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('menu should set when partyStateUpdate', async(() => {
+    mockPartyService.partyStateUpdate.emit({
+      id: 1,
+      phase: 0,
+      restaurant: null,
+      members: [],
+      menus: [mockPartyMenu1, mockPartyMenu2]
+    });
+    fixture.whenStable().then(() => {
+      expect(component.menus).toEqual([mockPartyMenu1, mockPartyMenu2]);
+    });
+  }));
+
+  it('menu should set when partyMenuCreate', async(() => {
+    mockPartyService.partyMenuCreate.emit([mockPartyMenu1, mockPartyMenu2]);
+    fixture.whenStable().then(() => {
+      expect(component.menus).toEqual([mockPartyMenu1, mockPartyMenu2]);
+    });
+  }));
+
+  it('getMenuNameById should return name if exist', async(() => {
+    fixture.whenStable().then(() => {
+      expect(component.getMenuNameById(1)).toEqual(mockMenu1.name);
+    });
+  }));
+
+  it('getMenuNameById should return empty string if not exist', async(() => {
+    fixture.whenStable().then(() => {
+      expect(component.getMenuNameById(2)).toEqual('');
+    });
+  }));
+
+  it('toggleAddMenu should toggle showAddMenuDialog', () => {
+    component.showAddMenuDialog = true;
+    component.toggleAddMenu();
+    expect(component.showAddMenuDialog).toBeFalsy();
+
+    component.showAddMenuDialog = false;
+    component.toggleAddMenu();
+    expect(component.showAddMenuDialog).toBeTruthy();
+  });
+
+  it('cancelAddMenu should set showAddMenuDialog to false', () => {
+    component.showAddMenuDialog = true;
+    component.cancelAddMenu(undefined);
+    expect(component.showAddMenuDialog).toBeFalsy();
+  });
+
+  it('requestAddMenu should set showAddMenuDialog to false and request', () => {
+    spyOn(mockPartyService, 'createMenu');
+    const mockPartyMenuCreateRequest = {
+      menuId: 2,
+      quantity: 1,
+      users: [1]
+    };
+
+    component.showAddMenuDialog = true;
+    component.requestAddMenu(mockPartyMenuCreateRequest);
+    expect(component.showAddMenuDialog).toBeFalsy();
+    expect(mockPartyService.createMenu)
+      .toHaveBeenCalledWith(mockPartyMenuCreateRequest);
+  });
+
+  it('updatePartyMenu should remove all when no people', async(() => {
+    spyOn(mockPartyService, 'updateMenu');
+    component.menus = [mockPartyMenu1, mockPartyMenu2];
+    fixture.whenStable().then(() => {
+      component.updatePartyMenu(mockPartyMenu1, -1, true);
+      expect(mockPartyService.updateMenu).toHaveBeenCalledWith({
+        id: mockPartyMenu1.id,
+        quantityDelta: -mockPartyMenu1.quantity,
+        addUserIds: [],
+        removeUserIds: mockPartyMenu1.userIds
+      });
+    });
+  }));
+
+  it('updatePartyMenu should remove me when people and quantity', async(() => {
+    spyOn(mockPartyService, 'updateMenu');
+    component.menus = [mockPartyMenu1, mockPartyMenu2];
+    fixture.whenStable().then(() => {
+      component.updatePartyMenu(mockPartyMenu2, -1, true);
+      expect(mockPartyService.updateMenu).toHaveBeenCalledWith({
+        id: mockPartyMenu2.id,
+        quantityDelta: -1,
+        addUserIds: [],
+        removeUserIds: [1]
+      });
+    });
+  }));
+
+  it('updatePartyMenu should add me when removing false', async(() => {
+    spyOn(mockPartyService, 'updateMenu');
+    component.menus = [mockPartyMenu3];
+    fixture.whenStable().then(() => {
+      component.updatePartyMenu(mockPartyMenu3, 1, false);
+      expect(mockPartyService.updateMenu).toHaveBeenCalledWith({
+        id: mockPartyMenu3.id,
+        quantityDelta: 1,
+        addUserIds: [1],
+        removeUserIds: []
+      });
+    });
+  }));
+
+  it('isUserId should return true when I am in', () => {
+    expect(component.isAssigned([3, 5, 1, 2])).toBeTruthy();
+  });
+
+  it('isUserId should return false when I am not in', () => {
+    expect(component.isAssigned([7, 2, 9, 4])).toBeFalsy();
+  });
+
 });
