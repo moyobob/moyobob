732808bff49102780f72b308c4ce25fd1ebf0038
pbzweihander <pbzweihander@gmail.com>
2018-11-28 20:03:21 +0900

    phase transition test

--- backend/websocket/tests/test_phase.py
+++ backend/websocket/tests/test_phase.py
@@ -2,12 +2,54 @@ from websocket import event
 from . import async_test, new_communicator
 from . import TestCaseWithSingleWebsocket, TestCaseWithDoubleWebsocket
 from api.models import Restaurant
+from websocket.models import PartyPhase
 
 
 class PhaseTestCase1(TestCaseWithSingleWebsocket):
     @async_test
     async def test_phase_transition(self):
-        pass
+        state = self.party.state
+
+        await self.join()
+
+        restaurant = Restaurant(name="Rustaurant")
+        restaurant.save()
+
+        await self.communicator.send_json_to({
+            'command': 'to.choosing.menu',
+            'restaurant_id': restaurant.id,
+        })
+        resp = await self.communicator.receive_json_from(1)
+        state.refresh_from_db()
+        self.assertDictEqual(resp, event.state_update(state))
+        self.assertEqual(state.phase, PartyPhase.ChoosingMenu)
+        self.assertEqual(state.restaurant_id, restaurant.id)
+
+        await self.communicator.send_json_to({
+            'command': 'to.ordering',
+        })
+        resp = await self.communicator.receive_json_from(1)
+        state.refresh_from_db()
+        self.assertDictEqual(resp, event.state_update(state))
+        self.assertEqual(state.phase, PartyPhase.Ordering)
+
+        await self.communicator.send_json_to({
+            'command': 'to.ordered',
+        })
+        resp = await self.communicator.receive_json_from(1)
+        state.refresh_from_db()
+        self.assertDictEqual(resp, event.state_update(state))
+        self.assertEqual(state.phase, PartyPhase.Ordered)
+
+        await self.communicator.send_json_to({
+            'command': 'to.payment',
+            'paid_user_id': self.user.id,
+        })
+        resp = await self.communicator.receive_json_from(1)
+        state.refresh_from_db()
+        self.assertDictEqual(resp, event.state_update(state))
+        self.assertEqual(state.phase, PartyPhase.PaymentAndCollection)
+        self.assertEqual(state.paid_user_id, self.user.id)
 
 
 class PhaseTestCase2(TestCaseWithDoubleWebsocket):
