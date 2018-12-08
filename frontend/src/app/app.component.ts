0f1b76f03e1dcb12bfc9a082bb3db0cd67426245
pbzweihander <pbzweihander@gmail.com>
2018-12-09 06:15:33 +0900

    always on open if not mobile

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Moyobob';
+  isSidebarOpened = false;
+
+  mobileQuery: MediaQueryList;
+  private mobileQueryListener: () => void;
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryListener = () => {
+      this.isSidebarOpened = !this.mobileQuery.matches;
+      this.changeDetectorRef.detectChanges()
+    };
+    this.mobileQuery.addListener(this.mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this.mobileQueryListener);
+  }
+
+  toggleSidebar(): void {
+    this.isSidebarOpened = !this.isSidebarOpened;
+    this.changeDetectorRef.detectChanges();
+  }
 }
