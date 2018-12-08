7667e45563b4e0e34ab546e4b46bcaf7b1b4d205
pbzweihander <pbzweihander@gmail.com>
2018-12-08 23:42:42 +0900

    postgresql setting

--- backend/test.sh
+++ backend/test.sh
@@ -2,8 +2,17 @@
 
 echo "Booting up redis..."
 redis=$(docker run -p 6379:6379 --rm -d redis:4-alpine)
+echo "$redis"
 
+echo "Booting up postgres..."
+postgres=$(docker run -p 5432:5432 -e --rm -d postgres:11-alpine)
+echo "$postgres"
+
+sleep 1
 coverage run --branch --source='./api,./websocket' manage.py test
 
 echo "Stopping redis..."
-docker stop "$redis" > /dev/null
+docker stop "$redis"
+
+echo "Stopping postgres..."
+docker stop "$postgres"
